928e53bb26e484cd6f5ae6bc9adca71458269db4
soulchild <soulchildsince2013@gmail.com>
2015-10-01 08:01:10 +0800

    dummy layout done

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,24 @@
+class StaticController < ApplicationController
+
+  layout "default"
+
+  #home page
+  def index
+  end
+
+  #team page
+  def teams
+  end
+
+  #schedule page
+  def schedule
+  end
+
+  #register page
+  def register
+  end
+
+  #about page
+  def about
+  end
+end
